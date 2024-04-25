07b38558306a6998c507573be8e7671ee43f2693
sifat26 <sifatict26@gmail.com>
2024-04-25 11:01:20 +0600

    get data from client side

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors= require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app =express();
 const port =process.env.PORT ||5000;
 app.use(cors());
@@ -10,8 +11,8 @@ app.get('/',(req,res)=>{
 // sifatict26
 //lTaZ75BrfXUeZhM9
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://sifatict26:<password>@cluster0.qvotocy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = "mongodb+srv://sifatict26:lTaZ75BrfXUeZhM9@cluster0.qvotocy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,12 +27,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    app.post('/users',async(req,res)=>{
+      const user=req.body;
+      console.log("New User",user);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
